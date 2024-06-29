70e3bd2af0309999e4d5afb50fce37ad7d0f7d85
Jared Hobson <jaredmhobson@gmail.com>
2024-06-29 00:04:15 -0700

    Add tests for happy path creating customer_subscription

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    description { "MyString" }
-    temperature { 150..190 }
-    brew_time { 1..5 }
+    title { Faker::Tea.variety }
+    description { Faker::Lorem.sentence(word_count: 10) }
+    temperature { Faker::Number.between(from: 150, to: 190) }
+    brew_time { Faker::Number.between(from: 1, to: 5) }
   end
 end
